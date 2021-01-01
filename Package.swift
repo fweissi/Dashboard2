5f1c1f32dd59de023f231f94b1c6ec267a1f5e3c
Keith Weiss <kweiss@brandwise.com>
2021-01-01 16:22:07 -0700

    Added Liquid Local Driver for use with Liquid and eventually S3

--- Package.swift
+++ Package.swift
@@ -8,6 +8,8 @@ let package = Package(
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
+        .package(url: "https://github.com/binarybirds/liquid.git", from: "1.1.0"),
+        .package(url: "https://github.com/binarybirds/liquid-local-driver.git", from: "1.1.0"),
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
@@ -16,6 +18,8 @@ let package = Package(
         .target(
             name: "App",
             dependencies: [
+                .product(name: "Liquid", package: "liquid"),
+                .product(name: "LiquidLocalDriver", package: "liquid-local-driver"),
                 .product(name: "Fluent", package: "fluent"),
                 .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
                 .product(name: "Vapor", package: "vapor")
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -6,10 +6,36 @@ import Vapor
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    app.routes.defaultMaxBodySize = "10mb"
 
     app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
 
     app.migrations.add(CreateTodo())
+    
+    app.post("upload") { (req) -> EventLoopFuture<String> in
+        let formData = try req.content.decode(FormData.self)
+//        let key = try req.query.get(String.self, at: "key")
+        struct FormData: Content {
+            let key: String
+            let file: Data
+        }
+        let path = req.application.directory.publicDirectory + "Images/" + formData.key
+        
+        return req.body.collect()
+            .unwrap(or: Abort(.noContent))
+            .flatMap { _ in req.fileio.writeFile(ByteBuffer(data: formData.file), at: path) }
+            .map { path }
+    }
+    
+    app.post("upload","image") { (req) -> EventLoopFuture<String> in
+        let key = try req.query.get(String.self, at: "key")
+        let path = req.application.directory.publicDirectory + "Images/" + key
+        
+        return req.body.collect()
+            .unwrap(or: Abort(.noContent))
+            .flatMap { req.fileio.writeFile($0, at: path) }
+            .map { key }
+    }
 
     // register routes
     try routes(app)

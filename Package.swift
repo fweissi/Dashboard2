07bc9f5311cafdf992f3f6df9e9896ada171f5ec
Keith Weiss <kweiss@brandwise.com>
2021-01-01 18:14:45 -0700

    Added Leaf to the project.

--- Package.swift
+++ Package.swift
@@ -10,9 +10,10 @@ let package = Package(
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/binarybirds/liquid.git", from: "1.1.0"),
         .package(url: "https://github.com/binarybirds/liquid-local-driver.git", from: "1.1.0"),
-        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.0.0"),
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/leaf.git", from: "4.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
     ],
     targets: [
         .target(
@@ -22,7 +23,8 @@ let package = Package(
                 .product(name: "LiquidLocalDriver", package: "liquid-local-driver"),
                 .product(name: "Fluent", package: "fluent"),
                 .product(name: "FluentSQLiteDriver", package: "fluent-sqlite-driver"),
-                .product(name: "Vapor", package: "vapor")
+                .product(name: "Leaf", package: "leaf"),
+                .product(name: "Vapor", package: "vapor"),
             ],
             swiftSettings: [
                 // Enable better optimizations when building in Release configuration. Despite the use of
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,12 +1,23 @@
 import Fluent
 import FluentSQLiteDriver
+import Leaf
+import Liquid
+import LiquidLocalDriver
 import Vapor
 
+let imageDirectory: String = "images"
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     app.routes.defaultMaxBodySize = "10mb"
+    
+    // using the local driver
+    app.fileStorages.use(.local(publicUrl: "http://localhost:8080/",
+                                publicPath: app.directory.publicDirectory,
+                                workDirectory: imageDirectory), as: .local)
+    
+    app.views.use(.leaf)
 
     app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
 
@@ -19,7 +30,7 @@ public func configure(_ app: Application) throws {
             let key: String
             let file: Data
         }
-        let path = req.application.directory.publicDirectory + "Images/" + formData.key
+        let path = req.application.directory.publicDirectory + imageDirectory + "/" + formData.key
         
         return req.body.collect()
             .unwrap(or: Abort(.noContent))
@@ -29,12 +40,20 @@ public func configure(_ app: Application) throws {
     
     app.post("upload","image") { (req) -> EventLoopFuture<String> in
         let key = try req.query.get(String.self, at: "key")
-        let path = req.application.directory.publicDirectory + "Images/" + key
+        let path = req.application.directory.publicDirectory + imageDirectory + "/" + key
         
         return req.body.collect()
             .unwrap(or: Abort(.noContent))
             .flatMap { req.fileio.writeFile($0, at: path) }
             .map { key }
+    }
+    
+    app.post("upload","test") { (req) -> EventLoopFuture<String> in
+        let key = try req.query.get(String.self, at: "key")
+        return req.body.collect()
+            .unwrap(or: Abort(.noContent))
+            .flatMap { req.fs.upload(key: key, data: Data(buffer: $0)) }
+        // returns the full public url of the uploaded image
     }
 
     // register routes

c8d00aaaef904a5760a46f0436efdcb8bf16a0ea
Keith Weiss <kweiss@brandwise.com>
2021-01-09 10:55:13 -0700

    Removed a route that is not longer supported.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -12,21 +12,6 @@ func routes(_ app: Application) throws {
         return "Hello, world!"
     }
     
-    app.post("upload","image") { (req) -> EventLoopFuture<String> in
-        let formData = try req.content.decode(FormData.self)
-//        let key = try req.query.get(String.self, at: "key")
-        struct FormData: Content {
-            let key: String
-            let file: Data
-        }
-        let path = req.application.directory.publicDirectory + imageDirectory + "/" + formData.key
-        
-        return req.body.collect()
-            .unwrap(or: Abort(.noContent))
-            .flatMap { _ in req.fileio.writeFile(ByteBuffer(data: formData.file), at: path) }
-            .map { path }
-    }
-    
     app.post("upload") { (req) -> EventLoopFuture<String> in
         var key = try req.query.get(String.self, at: "key")
         if key == "test.jpg" {
